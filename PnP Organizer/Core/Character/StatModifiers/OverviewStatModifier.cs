e2080713586d4604abcb3ff371f84e10cb201af7
Piggo41 <mr.piggo123@gmail.com>
2022-11-16 21:17:01 +0100

    added skill stat modifiers

---
+++ PnP Organizer/Core/Character/StatModifiers/OverviewStatModifier.cs
@@ -0,0 +1,18 @@
+﻿using PnP_Organizer.ViewModels;
+using System;
+using System.Reflection;
+
+namespace PnP_Organizer.Core.Character.StatModifiers
+{
+    public class OverviewStatModifier : StatModifier
+    {
+        public PropertyInfo StatPropertyInfo { get; private set; }
+        public int Bonus { get; private set; }
+
+        public OverviewStatModifier(string statPropertyName, int bonus) 
+        {
+            StatPropertyInfo = typeof(OverviewViewModel).GetProperty(statPropertyName)!;
+            Bonus = bonus;
+        }
+    }
+}
