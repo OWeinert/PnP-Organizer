49e32bc5e233c5c7ab7db3aba9bafd185b957951
Piggo41 <mr.piggo123@gmail.com>
2022-11-16 21:18:11 +0100

    removed ApplianceMode and DamageModifier and function AddDamageModifier, added StatModifiers array

--- PnP Organizer/Core/Character/Skill.cs
+++ PnP Organizer/Core/Character/Skill.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PnP_Organizer.Core.Character.StatModifiers;
+using System;
 using System.Drawing;
 
 namespace PnP_Organizer.Core.Character
@@ -8,20 +9,10 @@ namespace PnP_Organizer.Core.Character
         public string Name { get; set; }
         public string Description { get; set; }
         public SkillCategory SkillCategory { get; set; }
-
-        /// <summary>
-        /// Specifies if the skill modifies the base or the end damage.
-        /// </summary>
-        public ApplianceMode? ApplianceMode { get; private set; }
         public int SkillPoints { get; set; }
         public int MaxSkillPoints { get; set; }
 
-        /// <summary>
-        /// Delegate function which specifies how the player's damage is modified.
-        /// Func<float,float> parameter: float damage
-        ///                   returns: float modifiedDamage
-        /// </summary>
-        public Func<float, float>? DamageModifer { get; private set; }
+        public StatModifier[]? StatModifiers { get; private set; }
 
         /// <summary>
         /// Names of skills which need to be skilled in order to unlock this skill.
@@ -29,15 +20,13 @@ namespace PnP_Organizer.Core.Character
         // HACK Direct Skill references would be better, but maybe won't work with the SkillModel
         public string[] DependendSkillNames { get; private set; }
 
-        public Skill(string name, SkillCategory skillCategory, int maxSkillPoints, string description = "", string[] dependendSkillNames = null)
+        public Skill(string name, SkillCategory skillCategory, int maxSkillPoints, string description, StatModifier[]? statModifiers = null, string[]? dependendSkillNames = null)
         {
             Name = name;
             SkillCategory = skillCategory;
             Description = description;
             MaxSkillPoints = maxSkillPoints;
-
-            ApplianceMode = null;
-            DamageModifer = null;
+            StatModifiers = statModifiers;
             SkillPoints = 0;
 
             DependendSkillNames = dependendSkillNames != null ? dependendSkillNames : Array.Empty<string>();
@@ -48,12 +37,5 @@ namespace PnP_Organizer.Core.Character
         /// </summary>
         /// <returns></returns>
         public bool IsActive() => SkillPoints == MaxSkillPoints;
-
-        public Skill AddDamageModifier(Func<float, float> damageModifier, ApplianceMode applianceMode)
-        {
-            DamageModifer = damageModifier;
-            ApplianceMode = applianceMode;
-            return this;
-        }
     }
 }
