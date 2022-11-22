b9c3adfcaf6da747717596185a8aba3251371940
Piggo41 <mr.piggo123@gmail.com>
2022-11-22 21:58:19 +0100

    added SkillModelTemplateSelector

---
+++ PnP Organizer/Helpers/SkillModelTemplateSelector.cs
@@ -0,0 +1,21 @@
+﻿using PnP_Organizer.Models;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PnP_Organizer.Helpers
+{
+    public class SkillModelTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            FrameworkElement element = (FrameworkElement)container;
+
+            Debug.WriteLine(item.GetType());
+
+            if (item is RepeatableSkillModel) 
+                return (DataTemplate)element.FindResource("RepeatableSkillModelTemplate");
+            return (DataTemplate)element.FindResource("SkillModelTemplate");
+        }
+    }
+}
