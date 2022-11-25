7d1e2035eacc257f10437b9592f4db7f27cd890d
Piggo41 <mr.piggo123@gmail.com>
2022-11-25 19:41:43 +0100

    added InventoryArmorModel, added InventoryWeaponModel

---
+++ PnP Organizer/Models/InventoryArmorModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using PnP_Organizer.Core.Character.Inventory;
+
+namespace PnP_Organizer.Models
+{
+    public partial class InventoryArmorModel : InventoryItemModel
+    {
+        [ObservableProperty]
+        private int _armor = 0;
+        [ObservableProperty]
+        private float _putOnTime = 0.0f;
+        [ObservableProperty]
+        private float _weight = 1.0f;
+        [ObservableProperty]
+        private float _loudness = 0.0f;
+        [ObservableProperty]
+        private bool _isShield = false;
+
+        public InventoryArmorModel() : this (new InventoryArmor()) { }
+
+        public InventoryArmorModel(InventoryArmor inventoryArmor) : base(inventoryArmor)
+        {
+            IsInitialized = false;
+
+            Armor = inventoryArmor.Armor;
+            PutOnTime = inventoryArmor.PutOnTime;
+            Weight = inventoryArmor.Weight;
+            Loudness = inventoryArmor.Loudness;
+            IsShield = inventoryArmor.IsShield;
+
+            IsInitialized = true;
+        }
+    }
+}
