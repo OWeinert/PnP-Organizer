8b706f0fdf2f1ef0b36f51573918d02a28e84f1c
Piggo41 <mr.piggo123@gmail.com>
2022-12-05 23:35:54 +0100

    refined namespace structure

--- PnP Organizer/Core/Character/SkillSystem/Skills.cs
+++ PnP Organizer/Core/Character/SkillSystem/Skills.cs
@@ -1,4 +1,5 @@
 ﻿using PnP_Organizer.Core.Calculators;
+using PnP_Organizer.Core.Character.SkillSystem;
 using PnP_Organizer.Core.Character.StatModifiers;
 using PnP_Organizer.IO;
 using PnP_Organizer.Properties;
