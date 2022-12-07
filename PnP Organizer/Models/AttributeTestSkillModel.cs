16e2799476e791ccb9c02837633d85aae3bb9951
Piggo41 <mr.piggo123@gmail.com>
2022-12-07 20:36:56 +0100

    added AttributeTestSkillModel

---
+++ PnP Organizer/Models/AttributeTestSkillModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using PnP_Organizer.Core.Character;
+using PnP_Organizer.Core.Character.StatModifiers;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace PnP_Organizer.Models
+{
+    [INotifyPropertyChanged]
+    public partial class AttributeTestSkillModel : IModelCollectionItem
+    {
+        public ObservableCollection<IModelCollectionItem> ParentCollection { get; }
+        public Skill Skill { get; }
+        public AttributeTestStatModifier[] StatModifiers { get; }
+
+        [ObservableProperty]
+        private bool _isActive = false;
+
+        public AttributeTestSkillModel(Skill skill, ObservableCollection<IModelCollectionItem> parentCollection) 
+        {
+            Skill = skill;
+            StatModifiers = skill.StatModifiers!.Where(statModifier => statModifier is AttributeTestStatModifier).Cast<AttributeTestStatModifier>().ToArray();
+            ParentCollection = parentCollection;
+        }
+    }
+}
