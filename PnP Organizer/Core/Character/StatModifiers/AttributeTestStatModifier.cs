d52fed72143b966894ab0a60818195c788168e12
Piggo41 <mr.piggo123@gmail.com>
2022-12-07 20:36:37 +0100

    added Toggleable bool

--- PnP Organizer/Core/Character/StatModifiers/AttributeTestStatModifier.cs
+++ PnP Organizer/Core/Character/StatModifiers/AttributeTestStatModifier.cs
@@ -7,15 +7,17 @@ namespace PnP_Organizer.Core.Character.StatModifiers
         public string AttributeTestName { get; private set; }
         public Dice Dice { get; private set; }
         public int Bonus { get; private set; }
+        public bool Toggleable { get; private set; }
 
-        public AttributeTestStatModifier(string attributeTestName, int bonus) 
-            : this(attributeTestName, Dice.D1, bonus) { }
+        public AttributeTestStatModifier(string attributeTestName, int bonus, bool toggleable = false) 
+            : this(attributeTestName, Dice.D1, bonus, toggleable) { }
 
-        public AttributeTestStatModifier(string attributeTestName, Dice dice, int bonus = 0)
+        public AttributeTestStatModifier(string attributeTestName, Dice dice, int bonus = 0, bool toggleable = false)
         {
             AttributeTestName = attributeTestName;
             Dice = dice;
             Bonus = bonus;
+            Toggleable = toggleable;
         }
     }
 }
