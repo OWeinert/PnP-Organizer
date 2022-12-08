7830f0e6d80f6b9cfbd90ace218a9af211bcddfd
Piggo41 <mr.piggo123@gmail.com>
2022-12-08 22:17:20 +0100

    moved classes to expected namespaces

--- PnP Organizer/Core/Character/ProfessionSaveData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PnP_Organizer.Core.Character
-{
-    public struct ProfessionSaveData
-    {
-        public int AttributeTestIndex { get; set; }
-        public int Bonus { get; set; }
-    }
-}
