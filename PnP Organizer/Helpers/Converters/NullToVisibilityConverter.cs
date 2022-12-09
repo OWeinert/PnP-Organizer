6b91c4e3bae200910ead4d7848a51c0591f97816
Piggo41 <mr.piggo123@gmail.com>
2022-12-09 22:33:56 +0100

    added NullToVisibilityConverter

---
+++ PnP Organizer/Helpers/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PnP_Organizer.Helpers.Converters
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        // Somehow the value has to be null to set the visibility to hidden?
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value == null ? Visibility.Visible : Visibility.Hidden;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
