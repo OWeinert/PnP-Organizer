41f2a1e8f46c2463e0d086cdf2e379d12c8ebfa6
Piggo41 <mr.piggo123@gmail.com>
2022-12-09 23:20:27 +0100

    added AttackModeLocalizationConverter, IsShieldToLocalizationConverter and StringAddColonConverter

---
+++ PnP Organizer/Helpers/Converters/AttackModeLocalizationConverter.cs
@@ -0,0 +1,23 @@
+﻿using PnP_Organizer.Core.Calculators;
+using PnP_Organizer.Properties;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PnP_Organizer.Helpers.Converters
+{
+    public class AttackModeLocalizationConverter : IValueConverter
+    {
+        public AttackModeLocalizationConverter() { }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not AttackMode)
+                throw new ArgumentException("", nameof(value));
+
+            return Resources.ResourceManager.GetString($"Inventory_{(AttackMode)value}")!;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
---
+++ PnP Organizer/Helpers/Converters/StringAddColonConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PnP_Organizer.Helpers.Converters
+{
+    public class StringAddColonConverter : IValueConverter
+    {
+        // Somehow the value has to be null to set the visibility to hidden?
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.GetType() != typeof(string))
+                throw new ArgumentException("", nameof(value));
+
+            return $"{value}: ";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
