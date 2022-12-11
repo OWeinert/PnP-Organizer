9c48b4e0d1a100c556d041bedd01123a8792d252
Piggo41 <mr.piggo123@gmail.com>
2022-12-11 18:12:22 +0100

    added StatCalculators

---
+++ PnP Organizer/Core/BattleAssistant/StatCalculators.cs
@@ -0,0 +1,41 @@
+﻿using PnP_Organizer.Core.Character.Inventory;
+using PnP_Organizer.Core.Character.StatModifiers;
+using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace PnP_Organizer.Core.BattleAssistant
+{
+    public class StatCalculators
+    {
+
+        public static int CalculateDamage(InventoryWeapon weaponItem, List<CalculatorStatModifier> statModifiers)
+        {
+            return 0;
+        }
+
+        public static int CalculateHit(InventoryWeapon weaponItem, List<CalculatorStatModifier> statModifiers)
+        {
+            return 0;
+        }
+
+        public static int CalculateArmorpen(InventoryWeapon weaponItem, List<CalculatorStatModifier> statModifiers)
+        {
+            return 0;
+        }
+
+        public static int CalculateArmor(InventoryArmor armorItem, List<CalculatorStatModifier> statModifiers)
+        {
+            return 0;
+        }
+
+        public static int CalculateDodge(InventoryArmor armorItem, List<CalculatorStatModifier> statModifiers)
+        {
+            return 0;
+        }
+
+        public static int CalculateParry(InventoryArmor armorItem, List<CalculatorStatModifier> statModifiers)
+        {
+            return 0;
+        }
+    }
+}
