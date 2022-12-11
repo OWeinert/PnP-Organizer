64d3e7bf2b1e1a320293df1f8fbe2cbe378b1749
Piggo41 <mr.piggo123@gmail.com>
2022-12-11 18:11:22 +0100

    added InventoryShield and InventoryShieldModel

---
+++ PnP Organizer/Core/Character/Inventory/InventoryShield.cs
@@ -0,0 +1,26 @@
+﻿using PnP_Organizer.Core.BattleAssistant;
+using PnP_Organizer.Models;
+
+namespace PnP_Organizer.Core.Character.Inventory
+{
+    public class InventoryShield : InventoryItem
+    {
+        public int ParadeBonus { get; set; }
+        public Dice ParadeDiceBonus { get; set; }
+        public float Weight { get; set; }
+
+        public InventoryShield() : base()
+        {
+            ParadeBonus = 0;
+            ParadeDiceBonus = Dice.D4;
+            Weight = 1.0f;
+        }
+
+        public InventoryShield(InventoryShieldModel inventoryShieldModel) : base(inventoryShieldModel)
+        {
+            ParadeBonus = inventoryShieldModel.ParadeBonus;
+            ParadeDiceBonus = inventoryShieldModel.ParadeDiceBonus;
+            Weight = inventoryShieldModel.Weight;
+        }
+    }
+}
---
+++ PnP Organizer/Models/InventoryShieldModel.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using PnP_Organizer.Core.BattleAssistant;
+using PnP_Organizer.Core.Character.Inventory;
+using System.Windows;
+using System.Windows.Media;
+using Wpf.Ui.Common;
+
+namespace PnP_Organizer.Models
+{
+    public partial class InventoryShieldModel : InventoryItemModel
+    {
+        [ObservableProperty]
+        private int _paradeBonus = 0;
+        [ObservableProperty]
+        private Dice _paradeDiceBonus = Dice.D4;
+        [ObservableProperty]
+        private float _weight = 1.0f;
+
+        public InventoryShieldModel() : this (new InventoryShield()) { }
+
+        public InventoryShieldModel(InventoryShield inventoryShield) : base(inventoryShield)
+        {
+            IsInitialized = false;
+
+            ParadeBonus = inventoryShield.ParadeBonus;
+            ParadeDiceBonus = inventoryShield.ParadeDiceBonus;
+            Weight = inventoryShield.Weight;
+
+            Brush = (SolidColorBrush)Application.Current.Resources["PaletteBrownBrush"];
+
+            PropertyChanged += InventoryShieldModel_PropertyChanged;
+
+            IsInitialized = true;
+        }
+
+        private void InventoryShieldModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            var inventoryShield = (InventoryShield)InventoryItem;
+
+            inventoryShield.ParadeBonus = ParadeBonus;
+            inventoryShield.ParadeDiceBonus = ParadeDiceBonus;
+            inventoryShield.Weight = Weight;
+        }
+    }
+}
