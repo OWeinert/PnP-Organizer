3ab77c4119dc8b64c0288d02be27cd481ee1e6e7
Piggo41 <mr.piggo123@gmail.com>
2022-12-25 23:25:43 +0100

    added CalculatorStatResultToStringConverter

---
+++ PnP Organizer/Helpers/Converters/CalculatorStatResultToStringConverter.cs
@@ -0,0 +1,45 @@
+﻿using PnP_Organizer.Models;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace PnP_Organizer.Helpers.Converters
+{
+    public class CalculatorStatResultToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.GetType() != typeof(CalculatorStatResultModel))
+                throw new ArgumentException("", nameof(value));
+
+            var cValue = (CalculatorStatResultModel)value;
+
+            if(parameter != null && parameter.GetType() == typeof(string))
+            {
+                var sParameter = (string)parameter;
+
+                if(sParameter == "name")
+                {
+                    return $"{cValue.StatName}: ";
+                }
+                else if(sParameter == "value")
+                {
+                    return $"{cValue.StatValue}";
+                }
+                else if(sParameter == "diff" && cValue.StatDifference != 0)
+                {
+                    var optionalPlus = cValue.StatDifference > 0 ? "+" : string.Empty;
+                    return $"( {optionalPlus}{cValue.StatDifference} )";
+                }
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
