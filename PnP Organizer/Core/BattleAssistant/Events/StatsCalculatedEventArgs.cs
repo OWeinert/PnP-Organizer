7c8b291dec6628922d5b8b0a536ac971835096a1
Piggo41 <mr.piggo123@gmail.com>
2022-12-25 23:19:30 +0100

    added StatsCalculatedEventArgs

---
+++ PnP Organizer/Core/BattleAssistant/Events/StatsCalculatedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PnP_Organizer.Core.BattleAssistant.Events
+{
+    public class StatsCalculatedEventArgs : EventArgs
+    {
+        public BattleTurn BattleTurn { get; }
+
+        public StatsCalculatedEventArgs(BattleTurn battleTurn)
+        {
+            BattleTurn = battleTurn;
+        }
+    }
+}
