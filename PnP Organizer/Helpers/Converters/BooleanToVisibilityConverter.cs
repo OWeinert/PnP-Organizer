7e2dae4d4b5667fd4b2d2274aba342f846bd796a
Piggo41 <mr.piggo123@gmail.com>
2022-12-26 19:03:03 +0100

    added BooleanToVisibilityConverter

---
+++ PnP Organizer/Helpers/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PnP_Organizer.Helpers.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        // Somehow the value has to be null to set the visibility to hidden?
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.GetType() == typeof(bool))
+                throw new ArgumentException("", nameof(value));
+
+            return (bool)value ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
