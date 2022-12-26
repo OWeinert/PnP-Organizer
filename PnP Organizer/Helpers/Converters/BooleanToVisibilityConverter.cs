b6e8820be5eb872062d17037982c21f445c6de45
Piggo41 <mr.piggo123@gmail.com>
2022-12-26 20:09:40 +0100

    added invert paremeter

--- PnP Organizer/Helpers/Converters/BooleanToVisibilityConverter.cs
+++ PnP Organizer/Helpers/Converters/BooleanToVisibilityConverter.cs
@@ -10,9 +10,17 @@ namespace PnP_Organizer.Helpers.Converters
         // Somehow the value has to be null to set the visibility to hidden?
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value.GetType() == typeof(bool))
+            if (value.GetType() != typeof(bool))
                 throw new ArgumentException("", nameof(value));
 
+            if (parameter.GetType() != typeof(string))
+                throw new ArgumentException("", nameof(parameter));
+
+
+            if (bool.TryParse(parameter.ToString(), out bool bParameter) && bParameter)
+            {
+                return (bool)value ? Visibility.Hidden : Visibility.Visible;
+            }
             return (bool)value ? Visibility.Visible : Visibility.Hidden;
         }
 
