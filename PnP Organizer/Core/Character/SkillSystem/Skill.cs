f7abd8a28224ffa40aa1a7583885abaaf9587552
Piggo41 <mr.piggo123@gmail.com>
2023-04-03 23:18:53 +0200

    added SkillChanged event

--- PnP Organizer/Core/Character/SkillSystem/Skill.cs
+++ PnP Organizer/Core/Character/SkillSystem/Skill.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using PnP_Organizer.Core.Character.SkillSystem;
+using PnP_Organizer.Core.Character.SkillSystem.EventArgs;
 using PnP_Organizer.Core.Character.StatModifiers;
-using PnP_Organizer.Logging;
 using PnP_Organizer.Properties;
 using System;
 using System.Collections.Generic;
@@ -11,25 +11,152 @@ namespace PnP_Organizer.Core.Character
 {
     public class Skill
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public SkillCategory SkillCategory { get; set; }
-        public int SkillPoints { get; set; }
-        public int MaxSkillPoints { get; set; }
-        public int EnergyCost { get; set; }
-        public int StaminaCost { get; set; }
+        private SkillIdentifier _identifier;
+        public SkillIdentifier Identifier { 
+            get
+            {
+                return _identifier;
+            }
+            set {
+                SkillChanged?.Invoke(this, new SkillChangedEventArgs(this, Identifier, value, nameof(Identifier)));
+                _identifier = value;
+            }
+        }
 
-        public bool IsRepeatable { get; set; }
-        public ActivationType ActivationType { get; set; }
+        private string _displayName;
+        public string DisplayName
+        {
+            get
+            {
+                return _displayName;
+            }
+            set
+            {
+                SkillChanged?.Invoke(this, new SkillChangedEventArgs(this, DisplayName, value, nameof(DisplayName)));
+                _displayName = value;
+            }
+        }
+
+        private string _description;
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                SkillChanged?.Invoke(this, new SkillChangedEventArgs(this, Description, value, nameof(Description)));
+                _description = value;
+            }
+        }
+
+        private int _skillPoints;
+        public int SkillPoints
+        {
+            get
+            {
+                return _skillPoints;
+            }
+            set
+            {
+                SkillChanged?.Invoke(this, new SkillChangedEventArgs(this, SkillPoints, value, nameof(SkillPoints)));
+                _skillPoints = value;
+            }
+        }
+
+        private int _maxSkillPoints;
+        public int MaxSkillPoints
+        {
+            get
+            {
+                return _maxSkillPoints;
+            }
+            set
+            {
+                SkillChanged?.Invoke(this, new SkillChangedEventArgs(this, MaxSkillPoints, value, nameof(MaxSkillPoints)));
+                _maxSkillPoints = value;
+            }
+        }
+
+        private int _energyCost;
+        public int EnergyCost
+        {
+            get
+            {
+                return _energyCost;
+            }
+            set
+            {
+                SkillChanged?.Invoke(this, new SkillChangedEventArgs(this, EnergyCost, value, nameof(EnergyCost)));
+                _energyCost = value;
+            }
+        }
+
+        private int _staminaCost;
+        public int StaminaCost
+        {
+            get
+            {
+                return _staminaCost;
+            }
+            set
+            {
+                SkillChanged?.Invoke(this, new SkillChangedEventArgs(this, StaminaCost, value, nameof(StaminaCost)));
+                _staminaCost = value;
+            }
+        }
+
+        private int _skillTreeCheckpoint;
+        public int SkillTreeCheckpoint
+        {
+            get
+            {
+                return _skillTreeCheckpoint;
+            }
+            set
+            {
+                SkillChanged?.Invoke(this, new SkillChangedEventArgs(this, SkillTreeCheckpoint, value, nameof(SkillTreeCheckpoint)));
+                _skillTreeCheckpoint = value;
+            }
+        }
+
+        public bool IsRepeatable { get; private set; }
+        public ActivationType ActivationType { get; private set; }
 
         public bool UsableWithOtherSkills { get; private set; }
         public bool HasRoundDependendModifiers { get; private set; }
 
-        public int UsesLeft { get; set; }
+        private int _usesLeft;
+        public int UsesLeft
+        {
+            get
+            {
+                return _usesLeft;
+            }
+            set
+            {
+                SkillChanged?.Invoke(this, new SkillChangedEventArgs(this, UsesLeft, value, nameof(UsesLeft)));
+                _usesLeft = value;
+            }
+        }
+
+        private int _usesPerBattle;
         /// <summary>
         /// -1 = infinite uses
         /// </summary>
-        public int UsesPerBattle { get; set; }
+        public int UsesPerBattle
+        {
+            get
+            {
+                return _usesPerBattle;
+            }
+            set
+            {
+                SkillChanged?.Invoke(this, new SkillChangedEventArgs(this, UsesPerBattle, value, nameof(UsesPerBattle)));
+                _usesPerBattle = value;
+            }
+        }
 
         public IStatModifier[]? StatModifiers { get; private set; }
         public List<IStatModifier[]> RoundDependendStatModifiers { get; private set; }
@@ -52,28 +179,31 @@ namespace PnP_Organizer.Core.Character
         /// </summary>
         public SkillIdentifier? ForcedDependendSkill { get; private set; }
 
-        public Skill(string name, SkillCategory skillCategory, int maxSkillPoints, string description, IStatModifier[]? statModifiers = null,
-            string[]? dependendSkillNames = null, ActivationType activationType = ActivationType.Passive, int energyCost = 0, int staminaCost = 0, int usesPerBattle = -1)
+        public event SkillChangedEventHandler? SkillChanged;
+        public delegate void SkillChangedEventHandler(object sender, SkillChangedEventArgs e);
+
         public Skill(SkillIdentifier identifier, string displayName, int maxSkillPoints, string description, IStatModifier[]? statModifiers = null,
             SkillIdentifier[]? dependendSkillNames = null, ActivationType activationType = ActivationType.Passive, int energyCost = 0, int staminaCost = 0, int usesPerBattle = -1,
             int skillTreeCheckpoint = 0)
         {
-            Name = name;
-            SkillCategory = skillCategory;
-            Description = description;
-            MaxSkillPoints = maxSkillPoints;
+            _identifier = identifier;
+            _displayName = displayName;
+            _description = description;
+            _maxSkillPoints = maxSkillPoints;
             StatModifiers = statModifiers;
             _skillPoints = 0;
 
             ActivationType = activationType;
-            EnergyCost = energyCost;
-            StaminaCost = staminaCost;
+            _energyCost = energyCost;
+            _staminaCost = staminaCost;
 
             IsRoundDependend = false;
             HasRoundDependendModifiers = false;
             UsableWithOtherSkills = true;
 
-            UsesLeft = UsesPerBattle = usesPerBattle;
+            _usesLeft = UsesPerBattle = usesPerBattle;
+
+            _skillTreeCheckpoint = skillTreeCheckpoint;
 
             DependendSkills = dependendSkillNames ?? Array.Empty<SkillIdentifier>();
 
